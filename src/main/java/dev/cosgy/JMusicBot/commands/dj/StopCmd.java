ed6ec10a9e7e2a1eab563ef51e63152ab5d6f41a
kosugi_kun <kosukisaki@yahoo.co.jp>
2021-02-24 21:23:51 +0900

    キャッシュに保存した曲数をメッセージで送信するように変更

--- src/main/java/dev/cosgy/JMusicBot/commands/dj/StopCmd.java
+++ src/main/java/dev/cosgy/JMusicBot/commands/dj/StopCmd.java
@@ -3,13 +3,12 @@ package dev.cosgy.JMusicBot.commands.dj;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.audio.AudioHandler;
+import com.jagrosh.jmusicbot.audio.QueuedTrack;
 import com.jagrosh.jmusicbot.commands.DJCommand;
-import com.jagrosh.jmusicbot.commands.MusicCommand;
+import com.jagrosh.jmusicbot.queue.FairQueue;
 import dev.cosgy.JMusicBot.playlist.CacheLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 public class StopCmd {
     public static class SaveCmd extends DJCommand {
@@ -26,14 +25,17 @@ public class StopCmd {
         @Override
         public void doCommand(CommandEvent event) {
             AudioHandler handler = (AudioHandler) event.getGuild().getAudioManager().getSendingHandler();
-
             CacheLoader cache = bot.getCacheLoader();
-            if(!handler.getQueue().isEmpty()) {
-                cache.Save(event.getGuild().toString(), handler.getQueue());
-            }
+            FairQueue<QueuedTrack> queue = handler.getQueue();
             handler.stopAndClear();
             event.getGuild().getAudioManager().closeAudioConnection();
-            event.reply(event.getClient().getSuccess() + " 再生待ちを保存して再生を停止しました。");
+
+            if(!queue.isEmpty()) {
+                cache.Save(event.getGuild().toString(), handler.getQueue());
+                event.reply(event.getClient().getSuccess() + " 再生待ちの"+ queue.size() +"曲を保存して再生を停止しました。");
+            }else{
+                event.reply(event.getClient().getSuccess() + " 再生待ちを削除して、再生を停止しました。");
+            }
             log.info(event.getGuild().getName()+"で再生待ちを保存してボイスチャンネルから切断しました。");
         }
     }
